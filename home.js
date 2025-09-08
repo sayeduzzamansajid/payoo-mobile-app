0e4a76fca6c236d5ed9228ed95e1e40c398e1722
sayeduzzaman <sayeduzzamansajid@gmail.com>
2025-09-08 17:58:25 +0600

    cashout feature done

--- home.js
+++ home.js
@@ -1,3 +1,4 @@
+// add money function to add doller
 document.getElementById('btn-addMoney')
     .addEventListener('click', function (event) {
         event.preventDefault();
@@ -8,7 +9,7 @@ document.getElementById('btn-addMoney')
         // get available balance;
         const availableBalance = parseFloat(document.getElementById('available-balance').innerText);
         //checking pin number corrent or not 
-        if(inputPin !== '1234'){
+        if (inputPin !== '1234') {
             alert('Please, provide valid pin number')
             return;
         }
@@ -23,4 +24,47 @@ document.getElementById('btn-addMoney')
             alert('Please, provide valid account number')
             return;
         }
-    })
+    });
+
+// add money form event listner
+document.getElementById('add-money').addEventListener('click', function () {
+    // console.log('addmoney clicked')
+    const addMoneyForm = document.getElementById('add-money-form');
+    addMoneyForm.style.display = 'block';
+    const hide = document.getElementById('cash-out-form');
+    hide.style.display = 'none';
+});
+
+//cash out form event listner 
+document.getElementById('cash-out').addEventListener('click', function () {
+    // console.log('addmoney clicked')
+    const cashOutForm = document.getElementById('cash-out-form');
+    cashOutForm.style.display = 'block';
+    const chide = document.getElementById('add-money-form');
+    chide.style.display = 'none';
+});
+
+// withdraw money function to withdraw doller from wallet
+document.getElementById('btn-withdraw-money').addEventListener('click', function () {
+    const inputWithdraw = parseFloat(document.getElementById('withdraw-amount').value);//withdraw amount 
+    const wavailableBalance = parseFloat(document.getElementById('available-balance').innerText);//available balance 
+    const agentNumber = document.getElementById('agent-number').value
+    const winputPin = document.getElementById('wadd-pin').value;//pin number
+
+    //checking pin number corrent or not 
+    if (winputPin !== '1234') {
+        alert('Please, provide valid pin number')
+        return;
+    }
+    // checking account number is 11 digit or not 
+    if (agentNumber.length >= 11) {
+        //withdraw money calculation
+        const totalAmount = wavailableBalance - inputWithdraw;
+        // set available balance to wallet
+        document.getElementById('available-balance').innerText = totalAmount;
+    }
+    else {
+        alert('Please, provide valid account number')
+        return;
+    }
+});
