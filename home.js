b9009f820aaa5930ebb2f49b78aaa43733a97d0a
sayeduzzaman <sayeduzzamansajid@gmail.com>
2025-09-08 20:46:36 +0600

    added bill payment feature

--- home.js
+++ home.js
@@ -28,24 +28,16 @@ document.getElementById('btn-addMoney')
 
 // add money form event listner hide
 document.getElementById('add-money').addEventListener('click', function () {
-    // console.log('addmoney clicked')
     const addMoneyForm = document.getElementById('add-money-form');
-    addMoneyForm.style.display = 'block';
-    const hide = document.getElementById('cash-out-form');
-    hide.style.display = 'none';
     const transferForm = document.getElementById('transfer-form');
-    transferForm.style.display = 'none';
-});
-
-//cash out form event listner for hide 
-document.getElementById('cash-out').addEventListener('click', function () {
-    // console.log('addmoney clicked')
     const cashOutForm = document.getElementById('cash-out-form');
-    cashOutForm.style.display = 'block';
-    const chide = document.getElementById('add-money-form');
-    chide.style.display = 'none';
-    const transferForm = document.getElementById('transfer-form');
+    const getBonusForm = document.getElementById('get-bonus-form');
+    const payBillForm = document.getElementById('pay-bill-form');
+    addMoneyForm.style.display = 'block';
+    payBillForm.style.display = 'none';
     transferForm.style.display = 'none';
+    cashOutForm.style.display = 'none';
+    getBonusForm.style.display = 'none';
 });
 
 // withdraw money function to withdraw doller from wallet
@@ -79,6 +71,20 @@ document.getElementById('btn-withdraw-money').addEventListener('click', function
     }
 });
 
+//cash out form event listner for hide 
+document.getElementById('cash-out').addEventListener('click', function () {
+    const cashOutForm = document.getElementById('cash-out-form');
+    const transferForm = document.getElementById('transfer-form');
+    const addMoneyForm = document.getElementById('add-money-form');
+    const getBonusForm = document.getElementById('get-bonus-form');
+    const payBillForm = document.getElementById('pay-bill-form');
+    cashOutForm.style.display = 'block';
+    payBillForm.style.display = 'none';
+    transferForm.style.display = 'none';
+    addMoneyForm.style.display = 'none';
+    getBonusForm.style.display = 'none';
+});
+
 
 //transfer money event listner 
 
@@ -105,7 +111,8 @@ document.getElementById('btn-transfer').addEventListener('click', function (even
     if(inputPin !== pin){
         alert('Invalid pin!');return;
     }
-    if (inputPin === pin && accountNumber.length >= 11 && availableBalance > transferAmount) {
+    //transfer 
+    if (inputPin === pin && accountNumber.length >= 11 && availableBalance >= transferAmount) {
         const remainingBalance = availableBalance - transferAmount;
         //set the balance to wallet
         document.getElementById('available-balance').innerText = remainingBalance;
@@ -118,7 +125,70 @@ document.getElementById('transfer-money').addEventListener('click',function(even
     const transferForm = document.getElementById('transfer-form');
     const addMoneyForm = document.getElementById('add-money-form');
     const cashOutForm = document.getElementById('cash-out-form');
+    const getBonusForm = document.getElementById('get-bonus-form');
+    const payBillForm = document.getElementById('pay-bill-form');
     transferForm.style.display = 'block';
+    payBillForm.style.display = 'none';
+    addMoneyForm.style.display = 'none';
+    cashOutForm.style.display = 'none';
+    getBonusForm.style.display = 'none';
+});
+
+//get bonus function and event listner
+
+
+//get bonus form event for hide 
+
+document.getElementById('get-bonus').addEventListener('click',function(event){
+    const getBonusForm = document.getElementById('get-bonus-form');
+    const transferForm = document.getElementById('transfer-form');
+    const addMoneyForm = document.getElementById('add-money-form');
+    const cashOutForm = document.getElementById('cash-out-form');
+    const payBillForm = document.getElementById('pay-bill-form');
+    getBonusForm.style.display = 'block'
+    payBillForm.style.display = 'none';
+    transferForm.style.display = 'none';
     addMoneyForm.style.display = 'none';
     cashOutForm.style.display = 'none';
 })
+
+//pay Bill add event listner to pay bill
+
+document.getElementById('btn-pay-bill').addEventListener('click',function(event){
+    event.preventDefault();
+    const pin = '1234';
+    const availableBalance = parseFloat(document.getElementById('available-balance').innerText);
+    const payableBill = parseFloat(document.getElementById('bill-amount').value);
+    const inputPin = document.getElementById('bill-pin').value;
+    const accountNumber = document.getElementById('biller-acc-number').value;
+    const billType = document.getElementById('bill-type').value;
+    //pin check
+    if(inputPin !== pin){
+        alert('Invalid Pin!! Give The corrent pin');
+        return;
+    }
+    //balance check
+    if(availableBalance < payableBill){
+        alert('Insufficient Balance!!');
+        return
+    }
+    if(inputPin === pin && availableBalance > payableBill){
+        const remainingBalance = availableBalance - payableBill;
+        document.getElementById('available-balance').innerText = remainingBalance;
+    }
+});
+
+
+//event for Paybill and hide all 
+document.getElementById('transfer-money').addEventListener('click',function(event){
+    const payBillForm = document.getElementById('pay-bill-form');
+    const transferForm = document.getElementById('transfer-form');
+    const addMoneyForm = document.getElementById('add-money-form');
+    const cashOutForm = document.getElementById('cash-out-form');
+    const getBonusForm = document.getElementById('get-bonus-form');
+    payBillForm.style.display = 'block';
+    transferForm.style.display = 'none';
+    addMoneyForm.style.display = 'none';
+    cashOutForm.style.display = 'none';
+    getBonusForm.style.display = 'none';
+});
