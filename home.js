ca2677ba0dad7d6e102fa1fe4856d845eefdbc14
sayeduzzaman <sayeduzzamansajid@gmail.com>
2025-09-08 19:45:24 +0600

    transfer money added

--- home.js
+++ home.js
@@ -26,22 +26,26 @@ document.getElementById('btn-addMoney')
         }
     });
 
-// add money form event listner
+// add money form event listner hide
 document.getElementById('add-money').addEventListener('click', function () {
     // console.log('addmoney clicked')
     const addMoneyForm = document.getElementById('add-money-form');
     addMoneyForm.style.display = 'block';
     const hide = document.getElementById('cash-out-form');
     hide.style.display = 'none';
+    const transferForm = document.getElementById('transfer-form');
+    transferForm.style.display = 'none';
 });
 
-//cash out form event listner 
+//cash out form event listner for hide 
 document.getElementById('cash-out').addEventListener('click', function () {
     // console.log('addmoney clicked')
     const cashOutForm = document.getElementById('cash-out-form');
     cashOutForm.style.display = 'block';
     const chide = document.getElementById('add-money-form');
     chide.style.display = 'none';
+    const transferForm = document.getElementById('transfer-form');
+    transferForm.style.display = 'none';
 });
 
 // withdraw money function to withdraw doller from wallet
@@ -74,3 +78,47 @@ document.getElementById('btn-withdraw-money').addEventListener('click', function
         return;
     }
 });
+
+
+//transfer money event listner 
+
+document.getElementById('btn-transfer').addEventListener('click', function (event) {
+    event.preventDefault();
+    pin = '1234'
+    // get the available balence 
+    const availableBalance = parseFloat(document.getElementById('available-balance').innerText);
+    //get the transfered amount 
+    const transferAmount = parseFloat(document.getElementById('transfer-amount').value)
+    //get the pin
+    const inputPin = document.getElementById('t-pin').value;
+    //get the account number 
+    const accountNumber = document.getElementById('transfer-acc-number').value;
+    //balance checking 
+    if (availableBalance < transferAmount) {
+        alert("Insufficient Balance!!");return;
+    }
+    if(accountNumber.length < 11){
+        alert('Enver a valid account number')
+        return;
+    }
+    //pin checking 
+    if(inputPin !== pin){
+        alert('Invalid pin!');return;
+    }
+    if (inputPin === pin && accountNumber.length >= 11 && availableBalance > transferAmount) {
+        const remainingBalance = availableBalance - transferAmount;
+        //set the balance to wallet
+        document.getElementById('available-balance').innerText = remainingBalance;
+    }
+    
+})
+
+//event for transfer and hide all 
+document.getElementById('transfer-money').addEventListener('click',function(event){
+    const transferForm = document.getElementById('transfer-form');
+    const addMoneyForm = document.getElementById('add-money-form');
+    const cashOutForm = document.getElementById('cash-out-form');
+    transferForm.style.display = 'block';
+    addMoneyForm.style.display = 'none';
+    cashOutForm.style.display = 'none';
+})
